873986837f9a3eac214ec2919a156f55e14e9fb4
helenswanson <hhyswanson@gmail.com>
2014-06-19 15:29:35 -0400

    User can post question and view submitted questions in descending order.

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,38 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all.order(created_at: :desc)
+  end
+
+  def show
+
+
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      flash[:notice] = "Success!"
+      redirect_to '/questions'
+    else
+      flash.now[:notice] = "Your question couldn't be submitted."
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
+
+
+
+
+
+
+end
